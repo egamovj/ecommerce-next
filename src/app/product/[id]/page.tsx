a397501698a60e7d395d15d2d43fe23310b520a9
egamovj <egamovj90@gmail.com>
2024-05-15 17:15:27 +0500

    parallel routes

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { useParams } from "next/navigation";
+import { ProductType } from "@/interfaces";
 
 const ProductDetailedPage = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [loading, setLoading] = useState(false);
+  const [product, setProduct] = useState<ProductType>();
+  const { id } = useParams();
 
-export default ProductDetailedPage
+  useEffect(() => {
+    async function getData() {
+      setLoading(true);
+      const res = await fetch(`https://fakestoreapi.com/products/${id}`);
+      const product = await res.json();
+      setProduct(product);
+      setLoading(false);
+    }
+
+    getData();
+  }, [id]);
+
+  return <div></div>;
+};
+
+export default ProductDetailedPage;
